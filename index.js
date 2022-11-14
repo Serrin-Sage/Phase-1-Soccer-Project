44a32e316307c50434397508431ca57a2795d97a
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2022-11-14 14:15:50 -0500

    added more css, added event listener for clicking flags

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log("JS is running")
+
+//Get flag elements
+const flagContainer = document.getElementById("flag-container")
+
+//Get team card elements
 const teamCard = document.getElementById("card")
 const teamImage = document.getElementById("team-image")
 const teamCountry = document.getElementById("team-country")
@@ -7,6 +12,8 @@ const teamBestResult = document.getElementById("best-result")
 const teamCaptain = document.getElementById("captain")
 
 
+//Fetch database, for each object => render the team
+//set initial card to first object properties
 const fetchDB = () => {
     fetch("http://localhost:3000/world-cup")
     .then((res) => res.json())
@@ -14,14 +21,29 @@ const fetchDB = () => {
         data.forEach((item) => renderTeams(item));
         teamImage.src = data[0]["team_image"];
         teamCountry.textContent = data[0].country;
-        // teamRecentResult.textContent = data[0]["2018_result"];
-        teamBestResult.textContent = data[0].best;
+        teamRecentResult.textContent = `2018: ${data[0]["2018_result"]}`
+        teamBestResult.textContent = `Best: ${data[0].best}`;
         teamCaptain.textContent = data[0].captain;
     }))
 }
 
+
+//Render the team object properties 
+//When each flag is clicked, card display will change.
 const renderTeams = (teamObj) => {
-    
+    const flagImage = document.createElement("img")
+    flagImage.setAttribute("class", "flag-image")
+    flagImage.src = teamObj.flag;
+    flagContainer.append(flagImage)
+
+    flagImage.addEventListener("click", () => {
+        teamImage.src = teamObj["team_image"];
+        teamCountry.textContent = teamObj.country;
+        teamRecentResult.textContent = `2018: ${teamObj["2018_result"]}`;
+        teamBestResult.textContent = `Best: ${teamObj.best}`
+        teamCaptain.textContent = teamObj.captain;
+    })
+
 }
 
 fetchDB()
