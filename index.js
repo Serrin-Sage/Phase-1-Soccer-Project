8ea02fca35358a76ccb5c86d3aeec03918a41b0d
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2022-11-14 13:52:08 -0500

    started layout of page, added some css, rendered first object

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 console.log("JS is running")
-const teamListTest = document.getElementById("team-list-test")
+const teamCard = document.getElementById("card")
+const teamImage = document.getElementById("team-image")
+const teamCountry = document.getElementById("team-country")
+const teamRecentResult = document.getElementById("recent-result")
+const teamBestResult = document.getElementById("best-result")
+const teamCaptain = document.getElementById("captain")
+
 
 const fetchDB = () => {
     fetch("http://localhost:3000/world-cup")
     .then((res) => res.json())
     .then((data => {
-        data.forEach((item) => renderTeams(item))
+        data.forEach((item) => renderTeams(item));
+        teamImage.src = data[0]["team_image"];
+        teamCountry.textContent = data[0].country;
+        // teamRecentResult.textContent = data[0]["2018_result"];
+        teamBestResult.textContent = data[0].best;
+        teamCaptain.textContent = data[0].captain;
     }))
 }
 
 const renderTeams = (teamObj) => {
-    const teamItem = document.createElement("li")
-    teamItem.textContent = teamObj.team_image
-
-    teamListTest.append(teamItem)
+    
 }
 
 fetchDB()
